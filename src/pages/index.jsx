ce9093ca9d363abb2acf1fdc6f77c9ef09a54f61
BikerViking <160994481+BikerViking@users.noreply.github.com>
2025-06-26 05:19:40 -0400

    Remove Services section from homepage

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import LayoutWrapper from "../components/LayoutWrapper";
 import HeroSection from "../components/HeroSection";
-import ServicesSection from "../components/ServicesSection";
 
 export default function HomePage() {
   return (
     <LayoutWrapper>
       <HeroSection />
-      <ServicesSection />
     </LayoutWrapper>
   );
 }
