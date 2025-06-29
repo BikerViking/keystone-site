d46d527690980b5eeb87b59e4ab388bf8a096ccd
BikerViking <160994481+BikerViking@users.noreply.github.com>
2025-06-29 03:06:12 -0400

    style: improve mobile section backgrounds

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -17,7 +17,7 @@ export default function ContactPage() {
     <LayoutWrapper>
       <section
         aria-label="Contact"
-        className="mx-auto max-w-screen-lg px-4 py-12 text-gray-200 sm:px-6 sm:py-16 lg:px-8"
+        className="bg-neutral-800 mx-auto max-w-screen-lg px-4 py-12 text-gray-200 sm:px-6 sm:py-16 lg:px-8"
       >
         <h1 className="mb-8 text-center text-2xl font-semibold tracking-wide sm:mb-12 sm:text-3xl">
           Contact
--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -6,7 +6,7 @@ export default function ServicesPage() {
     <LayoutWrapper>
       <section
         aria-label="Services"
-        className="mx-auto max-w-screen-lg px-4 py-12 text-gray-200 sm:px-6 sm:py-16 lg:px-8"
+        className="bg-neutral-800 mx-auto max-w-screen-lg px-4 py-12 text-gray-200 sm:px-6 sm:py-16 lg:px-8"
       >
         <h1 className="mb-8 text-center text-2xl font-semibold tracking-wide sm:mb-12 sm:text-3xl">
           Our Services
