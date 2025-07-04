62d61a1d09baccb7808f4372c04256e576d831e8
BikerViking <160994481+BikerViking@users.noreply.github.com>
2025-07-03 23:00:18 -0400

    Remove unused tests and add basic App test

---
+++ src/App.test.jsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+import { HelmetProvider } from 'react-helmet-async';
+import App from './App';
+
+test('renders Keystone Notary Group heading', () => {
+  render(
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  );
+  const heading = screen.getByRole('heading', { name: /keystone notary group/i });
+  expect(heading).toBeInTheDocument();
+});
--- src/serviceWorkerRegistration.test.js
+++
@@ -1,6 +0,0 @@
-import { register, unregister } from './serviceWorkerRegistration';
-
-test('service worker helpers are functions', () => {
-  expect(typeof register).toBe('function');
-  expect(typeof unregister).toBe('function');
-});
