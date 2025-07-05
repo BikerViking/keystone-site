252ffef981408068442017b9e1928aa0b6edc95e
BikerViking <160994481+BikerViking@users.noreply.github.com>
2025-07-04 22:24:42 -0400

    Replace home page with test layout

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,12 +2,12 @@ import { render, screen } from '@testing-library/react';
 import { HelmetProvider } from 'react-helmet-async';
 import App from './App.jsx';
 
-test('renders Keystone Notary Group heading', () => {
+test('renders UNBOXED TEST heading', () => {
   render(
     <HelmetProvider>
       <App />
     </HelmetProvider>
   );
-  const heading = screen.getByRole('heading', { name: /keystone notary group/i, level: 1 });
+  const heading = screen.getByRole('heading', { name: /unboxed test/i, level: 1 });
   expect(heading).toBeInTheDocument();
 });
