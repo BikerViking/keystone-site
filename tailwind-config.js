5a08116f4efb4978bfb342563745c68a8caa838c
BikerViking <160994481+BikerViking@users.noreply.github.com>
2025-07-21 03:08:10 -0400

    fix: ensure dynamic colors

--- tailwind-config.js
+++ tailwind-config.js
@@ -4,7 +4,8 @@ module.exports = {
     './main.js',
     './calendar.js',
     './portal.js',
-    './theme.js'
+    './theme.js',
+    './src/document-verification.js'
   ],
   darkMode: 'class',
   theme: {
@@ -25,6 +26,9 @@ module.exports = {
   safelist: [
     'bg-charcoal', 'text-white', 'text-charcoal', 'bg-lightgray',
     'border-mediumgray', 'text-darkgray', 'text-platinum',
-    'text-mediumgray', 'opacity-70', 'cursor-not-allowed'
+    'text-mediumgray', 'opacity-70', 'cursor-not-allowed',
+    'bg-green-100', 'bg-yellow-100', 'bg-blue-100',
+    'text-green-600', 'text-green-800', 'text-yellow-600',
+    'text-yellow-800', 'text-blue-800', 'text-red-600'
   ]
 };
